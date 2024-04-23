59021d66dae89c561c29546069dfa9475f7db915
ADMIN <iVGeek>
2024-04-23 16:21:46 +0300

    Revert "Update gallery ID in fetch-gallery.ts"
    This reverts commit c07651599d0cdb5dcb164cf40fa9c606ab560625.

--- src/api/fetch-gallery.ts
+++
@@ -1,8 +0,0 @@
-const GALLERY_URL =
-  "https://api.buildingshistory.co.uk/api/v1/images/?gallery_id=4bc06f4d-b4ec-42c6-a641-a2666d3e5f1f";
-
-export const fetchGallery = async () => {
-  const request = await fetch(GALLERY_URL);
-  const data = await request.json();
-  return data;
-};
